3f9d4614982e79e1c53a532aa722a55b470be477
agastyam12 <83354866+agastyam12@users.noreply.github.com>
2021-08-06 18:51:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+var canvas = new fabric.Canvas("myCanvas");
+var block_image_width = 30;
+var block_image_height = 30;
+var player_x = 10;
+var player_y = 10;
+var player_object = "";
+
+
+
+function player_update(){
+
+     fabric.Image.fromURL("player.png" , function(Img)
+     {
+         player_object = Img;
+         player_object.scaleToWidth(150);
+         player_object.scaleToHeight(140);
+         player_object.set(
+             {
+                 top:player_y , 
+                 left:player_x 
+
+             }
+         );
+         canvas.add(player_object);    
+     });       
+}
